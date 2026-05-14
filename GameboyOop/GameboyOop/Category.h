bd35ed26e414933c9441e6a137e3298a110526dc
maham-fatima2205 <maham.fatima2205@gmail.com>
2026-05-14 20:35:34 +0500

    Update Category.h

--- GameboyOop/GameboyOop/Category.h
+++ GameboyOop/GameboyOop/Category.h
@@ -8,7 +8,6 @@ private:
 public:
     Category() { }
     Category(const char* name) {
-        // Copy name into `this->name`
         int i = 0;
         while (name[i] != '\0' && i < 49) {
             this->name[i] = name[i];
